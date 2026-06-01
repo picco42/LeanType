7b626f1c72d8ac8ae89ef8566bef756a22da7459
LeanBitLab <leanbitlab@users.noreply.github.com>
2026-06-01 12:23:59 +0530

    chore: bump version to 3.8.4
    Set versionCode to 3840 and versionName to 3.8.4 in build.gradle.kts. Create Fastlane changelog metadata at 3840.txt.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.leanbitlab.leantype"
         minSdk = 21
         targetSdk = 35
-        versionCode = 3830
-        versionName = "3.8.3"
+        versionCode = 3840
+        versionName = "3.8.4"
 
         proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         
